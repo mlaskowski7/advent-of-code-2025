aa332dd14f0329012fc39cf2bb3af1ed891d6e6b
mlaskowski7 <mtlaskowski7@gmail.com>
2025-12-01 17:49:13 +0100

    feat: day 1 part 2 solution

--- day1/day1.go
+++ day1/day1.go
@@ -6,7 +6,7 @@ import (
 	"github.com/mlaskowski7/advent-of-code-2025/utils"
 )
 
-func GetPassword() (int, error) {
+func GetPasswordPart1() (int, error) {
 	instructions, err := utils.ReadLines("day1/input.txt")
 	if err != nil {
 		return 0, err
@@ -26,9 +26,50 @@ func GetPassword() (int, error) {
 			curr -= diff
 		}
 
-		curr %= 100
+		curr = ((curr % 100) + 100) % 100
 		if curr == 0 {
 			password++
+		}
+	}
+
+	return password, nil
+}
+
+func GetPasswordPart2() (int, error) {
+	instructions, err := utils.ReadLines("day1/input.txt")
+	if err != nil {
+		return 0, err
+	}
+
+	password := 0
+	curr := 50
+	for _, instruction := range instructions {
+		diff, err := strconv.Atoi(instruction[1:])
+		if err != nil {
+			return 0, err
+		}
+
+		if diff > 100 {
+			password += diff / 100
+			diff %= 100
+		}
+
+		if instruction[0] == 'R' {
+			curr += diff
+			if curr > 99 {
+				password++
+				curr %= 100
+			}
+		} else {
+			wasZero := curr == 0
+			curr -= diff
+			if !wasZero && curr <= 0 {
+				password++
+			}
+
+			if curr < 0 {
+				curr += 100
+			}
 		}
 	}
 
--- main.go
+++ main.go
@@ -7,11 +7,18 @@ import (
 )
 
 func main() {
-	pass, err := day1.GetPassword()
+	pass, err := day1.GetPasswordPart1()
 	if err != nil {
-		fmt.Printf("An error occured while getting solution for day 1 task 1 - %v", err)
+		fmt.Printf("An error occured while getting solution for day 1 part 1 - %v", err)
 	} else {
 		fmt.Printf("Day 1, Part 1 answer is %d\n", pass)
+	}
+
+	pass, err = day1.GetPasswordPart2()
+	if err != nil {
+		fmt.Printf("An error occured while getting solution for day 1 part 2 - %v", err)
+	} else {
+		fmt.Printf("Day 1, Part 2 answer is %d\n", pass)
 	}
 
 }
